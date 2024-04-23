004ff4798f2384ac55bcb69a6e41a91d978e3849
ALIFYANFA <165451848+ALIFYANFA@users.noreply.github.com>
2024-04-23 17:21:33 +0700

    Add files via upload

--- mmol.py
+++ mmol.py
@@ -4,7 +4,7 @@ st.title('menghitung jumlah mmol dari suatu Gas mulia')
 
 tabl, tab2=st.tabs(['informasi tentang Gas mulia','informasi nilai Ar Gas Mulia'])
 
-with tab1:
+with tab1:alifyanfa/aplikasi-kalkulator 
     st.header('Informasi Tentang Gas Mulia')
     st.write('Gas Mulia atau Gas adi adalah Gas mulia adalah unsur kimia golongan 8 atau VIIIA di tabel periodik. Golongan ini disebut juga sebagai golongan helium atau neon dan golongan aerogen. Golongan ini terdiri dari unsur helium (He), Neon (Ne), argon (Ar), kripton (Kr), xenon (Xe), unsur radioaktif radon (Rn), dan unsur sintetis yang radioaktif oganeson (Og). Semua anggota unsur golongan 18 (VIIIA) merupakan gas mulia, bersifat nonlogam, dan berwujud gas pada suhu dan tekanan standar, kecuali oganeson (yang diprediksi akan berwujud padat dan bersifat seperti logam')
 
