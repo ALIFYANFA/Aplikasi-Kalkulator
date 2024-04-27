64f9026c3e9d41af0a5bae25e0643f9ddb5c1f9b
ALIFYANFA <165451848+ALIFYANFA@users.noreply.github.com>
2024-04-27 14:12:21 +0700

    Add files via upload

---
+++ kelompok.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.title('menghitung jumlah mmol dari suatu Alkali Tanah')
+
+tabl, tab2, tab3=st.tabs(['informasi tentang Alkali Tanah','informasi nilai Ar Alkali Tanah','kalkulator menghitung mmol'])
+
+with tab1:
+    st.header('informasi tentang alkali tanah')
+    st.write('Alkali tanah Alkali tanah merupakan keluarga unsur kimia yang menunjukkan pola dalam konfigurasi elektron. Secara keseluruhan, alkali tanah bersifat basa dan banyak ditemukan di dalam mineral tanah. Kelimpahan alkali tanah cukup banyak dan bentuknya berupa senyawa')
+
+with tab2:
+     st.header('Nilai Ar Alkali tanah')
+     st.write('Be =9,0122') 
+     st.write('Mg =24,312')
+     st.write('Ca =40,08')
+     st.write('Sr =87,62')
+     st.write('Ba =137,34')
+     st.write('Ra =226')
+
+with tab3:
+    st.header('kalkulator menghitung mmol')
+    options=st.multiselect(
+        'nama alkali tanah',
+        ['Be','Mg','Ca','Sr','Ba','Ra'])
+    y=st.number_input('Masukkan masa padatan dari unsur alkali tanah yang telah dipilih dalam satuan mg:')
+    y=st.number_input('Masukkan masa atom relatif alkali tanah (Ar) yang telah dipilih dalam satuan mg/mmol:')    
+      
