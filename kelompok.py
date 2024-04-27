bfcdf9863a7cc5451f43934a23b9c8283075ac9c
ALIFYANFA <165451848+ALIFYANFA@users.noreply.github.com>
2024-04-27 14:56:56 +0700

    Add files via upload

--- kelompok.py
+++ kelompok.py
@@ -5,17 +5,17 @@ st.title('menghitung jumlah mmol dari suatu Alkali Tanah')
 tabl, tab2, tab3=st.tabs(['informasi tentang Alkali Tanah','informasi nilai Ar Alkali Tanah','kalkulator menghitung mmol'])
 
 with tab1:
-     st.header('informasi tentang Alkali Tanah')
-     st.write('Alkali Tanah merupakan keluarga unsur kimia yang menunjukkan pola dalam konfigurasi elektron. Secara keseluruhan, alkali tanah bersifat basa dan banyak ditemukan di dalam mineral tanah. Kelimpahan alkali tanah cukup banyak dan bentuknya berupa senyawa')
+st.header('informasi tentang Alkali Tanah')
+st.write('Alkali Tanah merupakan keluarga unsur kimia yang menunjukkan pola dalam konfigurasi elektron. Secara keseluruhan, alkali tanah bersifat basa dan banyak ditemukan di dalam mineral tanah. Kelimpahan alkali tanah cukup banyak dan bentuknya berupa senyawa')
 
 with tab2:
-     st.header('Nilai Ar Alkali Tanah')
-     st.write('Be =9,0122') 
-     st.write('Mg =24,312')
-     st.write('Ca =40,08')
-     st.write('Sr =87,62')
-     st.write('Ba =137,34')
-     st.write('Ra =226')
+st.header('Nilai Ar Alkali Tanah')
+st.write('Be =9,0122') 
+st.write('Mg =24,312')
+st.write('Ca =40,08')
+st.write('Sr =87,62')
+st.write('Ba =137,34')
+st.write('Ra =226')
 
 with tab3:
     st.header('kalkulator menghitung mmol')
