8a7c34561a2cdeb9232111d998db22b180679192
ALIFYANFA <165451848+ALIFYANFA@users.noreply.github.com>
2024-04-27 14:41:10 +0700

    Add files via upload

--- kelompok.py
+++ kelompok.py
@@ -5,11 +5,11 @@ st.title('menghitung jumlah mmol dari suatu Alkali Tanah')
 tabl, tab2, tab3=st.tabs(['informasi tentang Alkali Tanah','informasi nilai Ar Alkali Tanah','kalkulator menghitung mmol'])
 
 with tab1:
-     st.header('informasi tentang alkali tanah')
-     st.write('Alkali tanah Alkali tanah merupakan keluarga unsur kimia yang menunjukkan pola dalam konfigurasi elektron. Secara keseluruhan, alkali tanah bersifat basa dan banyak ditemukan di dalam mineral tanah. Kelimpahan alkali tanah cukup banyak dan bentuknya berupa senyawa')
+     st.header('informasi tentang Alkali Tanah')
+     st.write('Alkali Tanah merupakan keluarga unsur kimia yang menunjukkan pola dalam konfigurasi elektron. Secara keseluruhan, alkali tanah bersifat basa dan banyak ditemukan di dalam mineral tanah. Kelimpahan alkali tanah cukup banyak dan bentuknya berupa senyawa')
 
 with tab2:
-     st.header('Nilai Ar Alkali tanah')
+     st.header('Nilai Ar Alkali Tanah')
      st.write('Be =9,0122') 
      st.write('Mg =24,312')
      st.write('Ca =40,08')
@@ -23,12 +23,12 @@ with tab3:
         'nama alkali tanah',
         ['Be','Mg','Ca','Sr','Ba','Ra'])
     y=st.number_input('Masukkan masa padatan dari unsur alkali tanah yang telah dipilih dalam satuan mg:')
-    y=st.number_input('Masukkan masa atom relatif alkali tanah (Ar) yang telah dipilih dalam satuan mg/mmol:')    
+    y=st.number_input('Masukkan masa atom relatif Alkali Tanah (Ar) yang telah dipilih dalam satuan mg/mmol:')    
 
 
 
 
-     tombol=st.button('Hitung jumlah mmol dari suatu alkali tanah')
+     tombol=st.button('Hitung jumlah mmol dari suatu Alkali Tanah')
      
      if tombol:
         jumlah_mmol=y/z
