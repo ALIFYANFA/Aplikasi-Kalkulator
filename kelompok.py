5f34ceea8b885cd8e6709579634b90758caed7dc
ALIFYANFA <165451848+ALIFYANFA@users.noreply.github.com>
2024-05-08 18:33:52 +0700

    Add files via upload

--- kelompok.py
+++ kelompok.py
@@ -1,35 +1,26 @@
 import streamlit as st
 
-st.title('menghitung jumlah mmol dari suatu Alkali Tanah')
+st.title('Menghitung Jumlah Molaritas dan PPM ')
 
-tab1, tab2, tab3=st.tabs(['informasi tentang Alkali Tanah','informasi nilai Ar Alkali Tanah','kalkulator menghitung mmol'])
+tab1, tab2, tab3=st.tabs(['informasi tentang PPM','kalkulator menghitung Molaritas','kalkulator menghitung ppm'])
 
 with tab1:
-    st.header('informasi tentang Alkali Tanah')
-    st.write('Alkali Tanah merupakan keluarga unsur kimia yang menunjukkan pola dalam konfigurasi elektron. Secara keseluruhan, alkali tanah bersifat basa dan banyak ditemukan di dalam mineral tanah. Kelimpahan alkali tanah cukup banyak dan bentuknya berupa senyawa')
+    st.header('informasi tentang PPM')
+    st.write('Satuan konsentrasi ppm (parts per million, "bagian per sejuta") adalah satuan yang dipakai sebagai satuan nirdimensi yang berasal dari pecahan yang sangat kecil, misalnya konsentrasi larutan atau kelimpahan partikel yang sangat kecil. ')
 
+    
 with tab2:
-    st.header('Nilai Ar Alkali Tanah')
-    st.write('Be =9,0122') 
-    st.write('Mg =24,312')
-    st.write('Ca =40,08')
-    st.write('Sr =87,62')
-    st.write('Ba =137,34')
-    st.write('Ra =226')
-
-with tab3:
-    st.header('kalkulator menghitung mmol')
-    options=st.multiselect(
-        'nama alkali tanah',
-        ['Be','Mg','Ca','Sr','Ba','Ra'])
-    y=st.number_input('Masukkan masa padatan dari unsur alkali tanah yang telah dipilih dalam satuan mg:')
-    z=st.number_input('Masukkan masa atom relatif Alkali Tanah (Ar) yang telah dipilih dalam satuan mg/mmol:')    
+    st.header('kalkulator menghitung Molaritas')
+    options=st.multiselect()
+    y=st.number_input('Masukkan masa padatan dari sampel yang telah dipilih dalam satuan mg:')
+    x=st.number_input('Masukkan bobot molekul sampel dalam satuan mmol/mL:')    
+    z=st.number_input('Masukkan volume dalam satuan mL')
 
 
 
 
-    tombol = st.button('Hitung jumlah mmol dari suatu Alkali Tanah')
+    tombol = st.button('Hitung jumlah molaritas')
      
     if tombol:
-        jumlah_mmol=y/z
-        st.success(f'jumlah mmol adalah{options}{jumlah_mmol}') 
+        jumlah_mmol=y/x*z
+        st.success(f'jumlah molaritas adalah{options}{jumlah_molaritas}') 
