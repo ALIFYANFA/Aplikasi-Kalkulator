f6e9acebabb567a6bb4f80c84fc37d37144f84b0
ALIFYANFA <165451848+ALIFYANFA@users.noreply.github.com>
2024-05-21 20:12:26 +0700

    Add files via upload

--- kelompok.py
+++ kelompok.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Kalkukator ppm dari Molaritas ')
 
-tab1, tab2, tab3, tab4=st.tabs(['Biodata Kelompok 7','Informasi Tentang ppm','Kalkulator ppm dari Molaritas','Rumus dan Contoh Soal'])
+tab1, tab2, tab3, tab4=st.tabs(['Biodata Kelompok 7','Informasi Tentang ppm','Rumus dan Contoh Soal','Kalkulator ppm dari Molaritas'])
 
 with tab1:
     st.header('Biodata kelompok')
@@ -12,9 +12,16 @@ with tab2:
     st.header('Informasi Tentang ppm')
     st.write('Satuan konsentrasi ppm (parts per million, "bagian per sejuta")pada dasarnya bertujuan untuk menghitung kadar kandungan yang terlarut dalam air, Dalam industri, ppm adalah satuan yang sering digunakan untuk mengukur kualitas produk dan bahan, serta kontrol kualitas dalam proses produksi, Penggunaan ppm memungkinkan untuk mendeteksi sejumlah sangat kecil dari bahan atau zat yang bisa berdampak signifikan terhadap lingkungan atau kesehatan manusia')
     st.image("gambar kimia orang.webp")
-    
+
 with tab3:
-    st.header('kalkulator ppm Dari Molaritas ')
+    st.header('Rumus dan Contoh Soal')
+    st.image('rumus molaritas - Salin.jpg')
+    st.image('rumus ppm.jpg')
+    st.image('contoh soal molaritas 1.jpg')
+    st.image('contoh soal ppm1.jpg')
+
+with tab4:
+    st.header('Kalkulator ppm dari Molaritas ')
     y=st.number_input('Masukkan masa sampel dalam satuan mg:')
     x=st.number_input('Masukkan bobot molekul sampel dalam satuan mg/mmol:')    
     z=st.number_input('Masukkan volume dalam satuan mL')
@@ -39,10 +46,3 @@ with tab3:
     if tombol:
         jumlahppm=x*y*z/v/0.001
         st.success(f'jumlah ppm adalah{jumlahppm}') 
-
-with tab4:
-    st.header('Rumus dan Contoh Soal')
-    st.image('rumus molaritas - Salin.jpg')
-    st.image('rumus ppm.jpg')
-    st.image('contoh soal molaritas 1.jpg')
-    st.image('contoh soal ppm1.jpg')       
